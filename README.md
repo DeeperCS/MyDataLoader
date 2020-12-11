28ec0039a7f3a17515a142750a6605fb6f36b0b8
Yao Zhou <zy3381@gmail.com>
2020-12-11 17:42:07 +0800

    my_data_loader

---
+++ README.md
@@ -0,0 +1,70 @@
+## Steps for generating batches with random sized images
+
+### Notice:
+##### The same class MyRandomResizeTransform must be imported in my_data_loader.py
+##### Since the variables IMAGE_SIZE_LIST and ACTIVE_SIZE are shared at class level
+```
+from dataset.MyRandomResizeTransform import MyRandomResizeTransform
+```
+
+### 1. Transform
+##### Constructs a transform class for resizing images
+```
+from dataset.MyRandomResizeTransform import MyRandomResizeTransform
+image_size = [160, 192, 224, 256]
+image_size.sort()  # e.g., 160 -> 224
+MyRandomResizeTransform.IMAGE_SIZE_LIST = image_size.copy()
+MyRandomResizeTransform.ACTIVE_SIZE = max(image_size)
+
+train_transforms = MyRandomResizeTransform(size=0)
+```
+
+
+### 2. Dataset 
+##### Applies the train_transforms to each of the loaded image
+```
+from dataset.dataset import MyDatasetTransform
+
+train_list = 'dataset/train.txt'
+train_dataset = MyDatasetTransform(train_list, train_transforms)
+```
+
+
+### 3. DataLoader
+##### Random set ACTIVE_SIZE for each batch
+
+```
+from dataset.my_data_loader import MyDataLoader
+train = MyDataLoader(train_dataset, batch_size=5, num_workers=4, pin_memory=True)
+```
+
+### 4. Use it
+##### Now every batch is in different size
+```
+for idx, batch in enumerate(train):
+    # print(batch)
+    print(batch['image'].shape)
+    if idx==10:
+        break
+  
+  
+output:
+torch.Size([5, 3, 192, 192])
+torch.Size([5, 3, 160, 160])
+torch.Size([5, 3, 160, 160])
+torch.Size([5, 3, 192, 192])
+torch.Size([5, 3, 160, 160])
+torch.Size([5, 3, 256, 256])
+torch.Size([5, 3, 192, 192])
+torch.Size([5, 3, 256, 256])
+torch.Size([5, 3, 256, 256])
+torch.Size([5, 3, 192, 192])
+torch.Size([5, 3, 160, 160])
+```
+
+
+
+
+[Reference]
+
+[Once for All](https://github.com/mit-han-lab/once-for-all)
---
+++ dataset.py
@@ -0,0 +1,120 @@
+import os
+from os.path import splitext
+from os import listdir
+import numpy as np
+from glob import glob
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms
+import logging
+from PIL import Image
+import random
+import json, cv2, time
+   
+class MyDatasetTransform(Dataset):
+    def __init__(self, img_list_path, transform, scale=1):
+        self.img_list_path = img_list_path
+        self.scale = scale
+        self.transform = transform
+        self.dataset = []
+        assert 0 < scale <= 1, 'Scale must be between 0 and 1'
+        
+        with open(img_list_path, "r") as f:
+            lines = f.readlines()
+        for line in lines:
+            triple = [x.strip() for x in line.split(',')]
+            self.dataset.append(triple)
+
+        logging.info(f'Creating dataset with {len(self.dataset)} examples')
+
+    def __len__(self):
+        return len(self.dataset)
+
+    @classmethod
+    def preprocess(cls, pil_img, scale, transform=None):
+        w, h = pil_img.size
+        newW, newH = int(scale * w), int(scale * h)
+        assert newW > 0 and newH > 0, 'Scale is too small'
+        pil_img = pil_img.resize((newW, newH))
+        
+        if transform!=None:
+            # print("transform", transform)
+            # Call Random Resize Transformation
+            pil_img = transform(pil_img)
+        # Data Augmentation
+        '''     
+        transform_compose = transforms.Compose([transforms.RandomHorizontalFlip(), \
+                                    transforms.RandomVerticalFlip(), \
+                                    transforms.RandomRotation(25)])
+        
+        
+           
+        transform_compose = transforms.Compose([transforms.RandomHorizontalFlip(), \
+                                    transforms.RandomVerticalFlip(), \
+                                    transforms.RandomRotation(25), \
+                                    transforms.RandomResizedCrop(300, scale=(0.7, 1.0)),
+                                    transforms.ColorJitter(brightness=(0.5,1.5), contrast=(0.5,1.5), saturation=(0.5,1.5))])
+                                    
+        
+        
+        if random.random()>0.5:
+            pil_img = transform_compose(pil_img)
+        '''
+        img_nd = np.array(pil_img)
+        # Remove alpha channel
+        if img_nd.shape[-1]==4:
+            img_nd = img_nd[:,:,0:3]
+
+        if len(img_nd.shape) == 2:
+            img_nd = np.expand_dims(img_nd, axis=2)
+
+        # HWC to CHW
+        img_trans = img_nd.transpose((2, 0, 1))
+        if img_trans.max() > 1:
+            img_trans = img_trans / 255
+
+        return img_trans
+
+    def __getitem__(self, idx):
+        triple = self.dataset[idx]
+        # import pdb; pdb.set_trace()
+        img_file = triple[0]
+        img_class = int(triple[1])
+        mask_file = triple[2]
+        
+        assert os.path.exists(img_file), \
+            f'Either no image or multiple images found for the ID {idx}: {img_file}'
+        img = Image.open(img_file)  ## .resize(self.fixed_size)
+        # mask = Image.open(mask_file[0])
+        # img = Image.open(img_file[0])
+        mask_torch = None
+        if img_class==1:
+            # print("img_class==1")
+            assert os.path.exists(mask_file), \
+                f'Either no mask or multiple masks found for the ID {idx}: {mask_file}'
+            mask = Image.open(mask_file)    ##  .resize(self.fixed_size)
+            
+            assert img.size == mask.size, \
+                f'Image and mask {idx} should be the same size, but are {img.size} and {mask.size}'
+            
+        
+        seed = random.randint(0,2**32)
+        random.seed(seed)
+        torch.manual_seed(seed)
+        img = self.preprocess(img, self.scale, self.transform)
+        img_torch = torch.from_numpy(img).type(torch.FloatTensor)
+        
+        if img_class==1:
+            random.seed(seed)
+            torch.manual_seed(seed)
+            mask = self.preprocess(mask, self.scale, self.transform)
+            mask_torch = torch.from_numpy(mask).type(torch.FloatTensor)
+            # print("mask_torch==1")
+        else:
+            mask_torch = torch.zeros(img_torch.shape[0], img_torch.shape[1]).type(torch.FloatTensor).unsqueeze(dim=0)
+
+        return {
+            'image': img_torch,
+            'class': torch.tensor(img_class).unsqueeze(dim=0).type(torch.FloatTensor),
+            'mask': mask_torch
+        }
